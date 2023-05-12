431eb2d0b6e479508f6e8cf449bbde39d09dbb26
Rahul-Knl <133352188+Rahul-Knl@users.noreply.github.com>
2023-05-12 15:51:08 +0530

    DB_MYSQL.py

--- DB_MYSQL.py
+++ DB_MYSQL.py
@@ -3,17 +3,17 @@ import mysql.connector
 # Connect to the MySQL server
 db = mysql.connector.connect(
     host="localhost",
-    user="python_test",
-    password="test123"
+    user="root",
+    password="rpsconsulting"
 )
 
 # Create a cursor object to interact with the database
 cursor = db.cursor()
 
 # Create a new database
-cursor.execute("CREATE DATABASE test")
+cursor.execute("CREATE DATABASE python_test")
 
-print("Database 'test' created successfully.")
+print("Database 'python_test' created successfully.")
 
 # Close the cursor and database connection
 cursor.close()
